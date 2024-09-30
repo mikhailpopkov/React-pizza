84de0914afb10bb20b2e97f4a2ad8a696636bfd7
mikhailpopkov <m.popkov.02@mail.ru>
2024-10-01 02:18:27 +0300

    Создал компоненты, прокинул пропсы и хуки

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React from "react";
+import "./scss/app.scss";
+import Header from "./components/Headeer";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
 function App() {
-  return;
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Чизбургер-пицца" price="395" />
+            <PizzaBlock title="Сырная" price="450" />
+            <PizzaBlock title="Креветки по-азиатски" price="290" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
