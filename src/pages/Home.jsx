9072c296f1ec58e87d9d5a9851b364ec10aa705c
mikhailpopkov <>
2024-11-29 18:24:45 +0300

    К предыдущему

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,20 +8,23 @@ import SkeletonHome from "../components/PizzaBlock/SkeletonHome";
 function Home() {
   const [items, setItems] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState(0);
 
   React.useEffect(() => {
-    fetch("https://672b0125976a834dd0253071.mockapi.io/items").then((array) =>
+    setLoading(true);
+    fetch(`https://672b0125976a834dd0253071.mockapi.io/items?category=${categoryId > 0 ? categoryId : ''}`).then((array) =>
       array.json().then((array) => {
         setItems(array);
         setLoading(false);
       })
     );
-  }, []);
+  }, [categoryId]);
 
   return (
     <>
       <div className="content__top">
-        <Categories />
+        <Categories value={categoryId} onClickCategory={(id) => setCategoryId(id)} />
         <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
